bdce57094dc553e46cc97d7a72d479956938e862
Norms16 <normsmiway@gmail.com>
2015-12-08 09:40:28 +0100

    Initial Commit

---
+++ AmazingTodo/Models/AmazingTodoContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace AmazingTodo.Models {
+    public class AmazingTodoContext : DbContext {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<AmazingTodoContext, Migrations.Configuration>());
+        }
+
+        public AmazingTodoContext() : base("name=AmazingTodoContext")
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
